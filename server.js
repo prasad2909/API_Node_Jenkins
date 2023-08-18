65ec2d000dea0b72ec92991aaa54eadfa510efa3
Prasad Gumati <gumati.prasad@gmail.com>
2023-08-18 16:18:00 +0530

    Landing Page API Modification

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('First API call......[1]')
+  res.send('First Added for Jenkins Retrigger Checking')
 })
 
 app.listen(port, () => {
